37d6cf3a83612bf31babef0548fc1b91b2f70fe8
Noah Sluss <slussnoah@gmail.com>
2015-08-17 16:15:05 -0700

    main.js reimplementation w/functional instantiation pattern implementation is more clean, readable, flexible, and as a bonus gives us private properties on our elements

--- example/app.js
+++ example/app.js
@@ -1,18 +1,28 @@
 var myString = "hey!"
-var myList = component({
-  name: 'myList',
-  init: function(tree, vals) {
-    return tree.toHtml()//tree.map(R.invoker(0, 'toUpperCase'))//.toHtml();//
-  },
-  template: div( {id:'testId'},
-              h1( {}, text(myString)),
-              ul( {},
-                li( {class:'test'}, text('item 1')),
-                li( {}, text('item 2'))
-              )
-            )
-})
-
-
-console.log(myList.render())
-document.querySelector('#main').innerHTML = myList.render()
+// var myList = component({
+//   name: 'myList',
+//   init: function(tree, vals) {
+//     return tree.toHtml()//tree.map(R.invoker(0, 'toUpperCase'))//.toHtml();//
+//   },
+//   template: div( {id:'testId'},
+//               h1( {}, text(myString)),
+//               ul( {},
+//                 li( {class:'test'}, text('item 1')),
+//                 li( {}, text('item 2'))
+//               )
+//             )
+// })
+//
+//
+// console.log(myList.render())
+// document.querySelector('#main').innerHTML = myList.render()
+var testNaive = div( {id:'testId'},
+                 [h1( {}, [text(myString)]),
+                  ul( {}, [
+                    li( {class:'test'}, [text('item1')]),
+                    li( {}, [text('item2')])
+                  ])
+                ])
+console.log(div({id:'testId'},[text('test')]).toHtml())
+console.log(testNaive.toHtml());
+document.querySelector('#main').innerHTML = testNaive.toHtml();
--- src/main.js
+++ src/main.js
@@ -1,103 +1,29 @@
-var descendant = R.curry(function (constructor, child) {
-  return constructor.prototype.isPrototypeOf(child);
-})
-
-
-function Tree (val) {
-  this.value = val || '';
-}
-
-function Leaf (val) {
-  return new Tree(val)
-}
-
-function Node (val, children) {
-  var base = new Tree(val)
-  base.children = children || [];
-  return base;
-}
-
-function Text (value) {
-  this.value = value
-}
-
 function element (name) {
-  return function (value) {
-    return new Element (name, value, sliceArgs(1, arguments));
+  var el = function (attrs, children) {
+    var children = children;
+    var attrs = attrs;
+    return {
+      toHtml: function () {
+        return '<' + name + stringifyAttrs(attrs) + '>' +
+               (children ? children.map(R.invoker(0, 'toHtml')).join('') : '') +
+               '</' + name +'>'
+      }
+    }
   }
-};
-
-
-function Element (name, value, children) {
-  var parent = {};
-  var obj;
-  if (children.length > 0) {
-    obj = Object.create (Node.prototype)
-    obj.value = value;
-    obj.children = children;
-  } else if (descendant(Text, value)) {
-    obj = Text.call({}, value);
-  } else {
-    obj = Object.create(Leaf.prototype)
-    obj.value = value;
-  }
-  this.name = name;
-  this.value = obj;
-};
-
-Element.prototype.toHtml = function () {
-  return this.name === 'text' ?
-          this.value.toStr() :
-          descendant(Node, this.value) ?
-            '<' + this.name + stringifyAttrs(this.value.value) + '>' +
-            this.value.children.map(R.invoker(0, 'toHtml')).join('') +
-            '</' + this.name + '>' :
-            // Leaf
-            '<' + this.name + '>' +
-            this.value.toStr() +
-            '</' + this.name + '>'
-};
-
-Tree.prototype.empty = function () {
-  return new Leaf ();
-};
-Tree.prototype.toStr = function () {
-  return this.value;
+  return el;
 }
 
-Leaf.prototype = Object.create(Tree.prototype);
-Leaf.prototype.constructor = Leaf;
-Leaf.prototype.map = function (fn) {
-  return new Leaf(fn(this.value));
-};
-Leaf.prototype.concat = function (tree) {
-  return new Node(this.value, R.of(tree));
-}
-
-
-Node.prototype = Object.create(Tree.prototype);
-Node.prototype.constructor = Node;
-Node.prototype.map = function (fn) {
-  return new Node(fn(this.value), this.children.map(R.invoker(1, 'map')(fn)))
-};
-Node.prototype.concat = function (tree) {
-  return new Node(this.value, R.concat(this.children, R.of(tree)))
-};
-
-Text.prototype = Object.create(Leaf.prototype);
-Text.constructor = Text;
-
-
-Element.prototype.map = function(fn) {
-  var res = fn(this.value.map(fn))
-  return element(this.name, res);
+function text (val) {
+  return {
+    toHtml: function () { return val }
+  }
 }
 
 var div  = element('div'),
     h1   = element('h1'),
     ul   = element('ul'),
-    li   = element('li'),
-    text = element('text')
+    li   = element('li')
+    //text = element('text')
 
 function sliceArgs (from, args) {
   return Array.prototype.slice.call(args, from);
