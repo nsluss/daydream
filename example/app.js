53974918e823c450b91fa8971adf83f39f3cc14e
Noah Sluss <slussnoah@gmail.com>
2015-09-29 17:44:05 -0700

    pushing latest changes

--- example/app.js
+++ example/app.js
@@ -17,12 +17,19 @@ var myString = "hey!"
 // console.log(myList.render())
 // document.querySelector('#main').innerHTML = myList.render()
 var testNaive = div( {id:'testId'},
-                 [h1( {}, [text(myString)]),
-                  ul( {}, [
-                    li( {class:'test'}, [text('item1')]),
-                    li( {}, [text('item2')])
-                  ])
-                ])
-console.log(div({id:'testId'},[text('test')]).toHtml())
-console.log(testNaive.toHtml());
+                 h1( {}, text(myString)),
+                  ul( {},
+                    li( {class:'test'}, text('item1')),
+                    li( {}, text('item2'))
+                  )
+                )
+
+
+console.log(div({id:'testId'}, [text('test')]).map(toUp)())
+console.log(testNaive.map(toUp).toHtml());
 document.querySelector('#main').innerHTML = testNaive.toHtml();
+
+function toUp (node) {
+  return {name:'div', attrs:{}, children:node.children, text:node.text.toUpperCase()}
+  return {name:node.name, attrs:node.attrs, children:(node.name === 'text' ? node.children.toUpperCase() : node.children)};
+}
--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 function element (name) {
-  var el = function (attrs, children) {
-    var children = children;
+  var el = function (attrs, children, text) {
+    var children = sliceArgs(1, arguments);
     var attrs = attrs;
     return {
       toHtml: function () {
         return '<' + name + stringifyAttrs(attrs) + '>' +
+               text +
                (children ? children.map(R.invoker(0, 'toHtml')).join('') : '') +
                '</' + name +'>'
+      },
+      map: function (fn) {//broken
+
+        return newEl(fn({attrs: attrs, name: name, children: children, text: text || ''}))
       }
     }
   }
   return el;
 }
 
-function text (val) {
+function text (val) { //alias for this and other elements to allow instantiation by passing an object
   return {
-    toHtml: function () { return val }
+    toHtml: function () { return val },
+    map: function () {return newEl(fn({attrs: {}, name:'text', children: [], text: text || ''}))} //setting children as val may not be correct
   }
+
+}
+
+function newEl (description) { //broken?
+  return (description.name === 'text' ?
+    text(description.text) :
+    R.curry(element(description.name))(description.attrs)
+      .apply(Object.create(null), description.children, description.text)) //can't curry, but need to keep the .apply() in order to pass a list instead of an array, but that doesn't work if we want to pass text as an argument to all of our element functions. this may be where we need to start using applicative/functor/monad instances (lift?)
 }
 
 var div  = element('div'),
